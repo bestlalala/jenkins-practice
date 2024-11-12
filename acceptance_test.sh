623eab1f3b38ef9eb7ef7261deb575ef0f9caede
yeonsu <bestdustn@sookmyung.ac.kr>
2024-11-12 14:23:17 +0900

    [Update] acceptance_test.sh - 디버깅 코드 추가

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-test $(curl "http://localhost:8765/sum?a=10&b=3") -eq 13
+response=$(curl -s "http://localhost:8765/sum?a=10&b=3")
+echo "Server response: $response"
+
+if [ "$response" -eq 13 ]; then
+  echo "Test passed"
+else
+  echo "Test failed"
+  exit 1
+fi
